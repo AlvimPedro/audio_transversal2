49e27ca13582191388b5d218169adef390fb6522
AlvimPedro <pedroalvim2000@hotmail.com>
2021-09-04 16:55:24 -0300

    Script que cria o csv do dataset

---
+++ csv_dataset.py
@@ -0,0 +1,20 @@
+import os
+
+dir = './audio_dataset_cortado'
+
+f = open('audio_dataset.csv', 'w')
+f.write('audio_path,label\n')
+f.close()
+
+f = open('audio_dataset.csv', 'a')
+
+for diretorio, subpastas, arquivos in os.walk(dir):
+    for arquivo in arquivos:
+        path = os.path.join(diretorio, arquivo)
+        label = arquivo.split('_')[0]
+
+        line = path+','+label+'\n'
+
+        f.write(line)
+        
+f.close()
