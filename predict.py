3426fdd7ee94360ceef8e00230f4d16adbcc350f
AlvimPedro <pedroalvim2000@hotmail.com>
2021-10-06 15:41:58 -0300

    Código do mysql, mas precisa criar o BD

--- predict.py
+++ predict.py
@@ -4,9 +4,23 @@ import threading
 
 import gravarAudio as gravar
 
-model = LudwigModel.load('./results/experiment_run_5/model/')
+import mysql.connector
 
-def thread_prediction():
+def mysql_data_send(sql, val):
+    mydb = mysql.connector.connect(
+        host="localhost",
+        user="yourusername",
+        password="yourpassword",
+        database="mydatabase"
+    )
+
+    mycursor = mydb.cursor()
+
+    mycursor.execute(sql, val)
+
+    mydb.commit()
+
+def thread_prediction(model):
     predictions, output_directory = model.predict(dataset='./audio_predict.csv')   
 
     print(predictions.label_probabilities_0[0])
@@ -14,11 +28,22 @@ def thread_prediction():
     print(predictions.label_probabilities_2[0])
     print(predictions.label_probabilities_4[0])
 
-while True:
-    gravar.gravarAudio(10, './prediction/gravacao.wav')
+    sql = "INSERT INTO customers (name, address) VALUES (%s, %s)"
+    val = ("John", "Highway 21")   
 
-    threading.Thread(target=thread_prediction).start()
-    
+    mysql_data_send(sql, val)
+
+def main():
+    model = LudwigModel.load('./results/experiment_run_5/model/')
+
+    while True:
+        gravar.gravarAudio(10, './prediction/gravacao.wav')
+
+        threading.Thread(target=thread_prediction(model)).start()
+
+if __name__ == '__main__':
+    main()
+        
     
 
     
