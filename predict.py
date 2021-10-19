fb72b35cead7eec3596cb5e94ba753cfaf157529
AlvimPedro <pedroalvim2000@hotmail.com>
2021-10-19 20:35:24 -0300

    Conexão com o bd e usabilidade do predict.py

--- predict.py
+++ predict.py
@@ -6,12 +6,24 @@ import gravarAudio as gravar
 
 import mysql.connector
 
+from datetime import datetime
+
+df = open('./info_coletora.txt','r')
+linha = df.readline()
+linha = linha.split()
+
+id_coletora = linha[0]
+latitude = linha[1]
+longitude = linha[2]
+
+df.close()
+
 def mysql_data_send(sql, val):
     mydb = mysql.connector.connect(
-        host="localhost",
-        user="yourusername",
-        password="yourpassword",
-        database="mydatabase"
+        host='192.185.210.165',
+        user='engne598',
+        password='ges2019direx@)!(',
+        database='engne598_coletora_alerta_db'
     )
 
     mycursor = mydb.cursor()
@@ -20,18 +32,44 @@ def mysql_data_send(sql, val):
 
     mydb.commit()
 
+
+
+
 def thread_prediction(model):
     predictions, output_directory = model.predict(dataset='./audio_predict.csv')   
+
+    # print(predictions.info())
+    # for i in predictions.label_probabilities:
+    #     print(i)
+    # print(predictions.label_probabilities)
+    # print(predictions.label_probability[0])
+    # # print(predictions.label_probabilities_<UNK>)
+
+    status_tiro = '0'
+    status_serra = '0'
+
+    if predictions.label_probabilities_0[0] > 0.7:
+        status_serra = '1'
+    
+    elif predictions.label_probabilities_1[0] > 0.5 or predictions.label_probabilities_2[0] > 0.5 or predictions.label_probabilities_4[0] > 0.5:
+        status_tiro = '1'
+
 
     print(predictions.label_probabilities_0[0])
     print(predictions.label_probabilities_1[0])
     print(predictions.label_probabilities_2[0])
     print(predictions.label_probabilities_4[0])
+    #print(predictions.label_probabilities_5[0])
 
-    sql = "INSERT INTO customers (name, address) VALUES (%s, %s)"
-    val = ("John", "Highway 21")   
+    data = str(datetime.now())
+
+    sql = "INSERT INTO dados (id_coletora, status_tiro, status_serra, latitude, longitude, data) VALUES (%s, %s, %s, %s, %s, %s)"
+    val = (id_coletora, status_tiro, status_serra, latitude, longitude, data)   
 
     mysql_data_send(sql, val)
+
+
+
 
 def main():
     model = LudwigModel.load('./results/experiment_run_5/model/')
